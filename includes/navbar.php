fbd02b56aac50a2ca8517ca74ee972f7de1c4053
Gabriel Teruel <coledis85@gmail.com>
2022-06-28 16:45:54 -0300

    hago menu automatico

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,25 +1,28 @@
 <?php
-//$img = ($_SERVER['PHP_SELF'] == "/nuestrosproductos.php") ? "logoproductos.jpg" : "log.png";
-$img = ($_SERVER['PHP_SELF'] == "/nuestrosproductos.php") ? "logoproductosnow2.png" : "log.png";
+$items = [
+    'HOME' => 'index.php',
+    'OFERTAS' => 'ofertas.php',
+    'AEREOS' => 'aereos.php',
+    'QUIENES SOMOS' => 'quienesSomos.php',
+    'NUESTROS PASAJEROS' => 'nuestrosPasajeros.php',
+    'RESERVAS ONLINE' => 'buscador.php',
+    'CONTACTENOS' => 'contacto.php'
+];
 ?>
 <nav class="mnu">
     <div class="container1">
         <div class="logo" id="logo">
             <h1>
-                <a id="a_navbar" href="index.php"><img src="imagenes/<?php echo $img; ?>"></a>
+                <a id="a_navbar" href="index.php"><img src="imagenes/log.png"></a>
             </h1>
         </div>
         <label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
         <input type="checkbox" id="drop">
         <ul class="menu mt-1">
-            <li class="mr-lg-4 mr-3 active"><a href="index.php">HOME</a></li>
-            <li class="mr-lg-4 mr-3"><a href="ofertas.php" class="scroll">OFERTAS</a></li>
-            <li class="mr-lg-4 mr-3"><a href="aereos.php" class="scroll">AEREOS</a></li>
-            <li class="mr-lg-4 mr-3"><a href="quienesSomos.php" class="scroll">QUIENES SOMOS</a></li>
-            <li class="mr-lg-4 mr-3"><a href="nuestrosPasajeros.php" class="scroll">NUESTROS PASAJEROS</a></li>
-            <li class="mr-lg-4 mr-3" id="buscador"><a href="buscador.php" class="scroll">RESERVAS ONLINE</a>
-            <li class="mr-lg-4 mr-3"><a href="contacto.php" class="scroll">CONTACTENOS</a>
-            </li>
+            <?php foreach ($items as $title => $link) {
+                $active = ($_SERVER['PHP_SELF'] == "/" . $link) ? " active" : ""; ?>
+                <li class="mr-lg-4 mr-3<?php echo $active; ?>"><a href="<?php echo $link; ?>"><?php echo $title; ?></a></li>
+            <?php } ?>
         </ul>
     </div>
 </nav>
