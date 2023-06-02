4133533b0a3b0055b7fef41fdb6a653867c5dd4c
Gabriel Teruel <coledis85@gmail.com>
2023-06-02 09:17:23 -0300

    agrego buscaro de vuelos de carnival

--- aereos_aerotur.php
+++ aereos_aerotur.php
@@ -12,14 +12,12 @@
     </header>
 
     <main>
-        <div class="formAereos" data-tipoMotor="aereos"></div>
+        <iframe title="Check_in" src="https://la_catrina.mbc.aero.tur.ar/verticalsearchbar" height="430" width="100%" kwframeid="1" style="border: none;box-shadow:  0 3px 4px 0 rgba(255, 0, 0, 0.2);"></iframe>
     </main>
 
     <footer>
         <?php include_once "includes/footer.php"; ?>
     </footer>
-
-    <script src="//carnival.aereos.app/aereos.app.min.js"></script>
 
 </body>
 
